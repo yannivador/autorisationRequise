6f32282df9095ecb03483f682fda19a1c4301d86
yannivador <yann901@hotmail.fr>
2022-01-13 15:42:43 +0400

    test

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
 
 
-function fonction() {
-	var age2 = document.getElementById("nombre").value;
+function login() {
+	var age = document.getElementById("nombre").value;
 
-	if (isNaN(age2)) {
+	if (isNaN(age)) {
 		alert('Vous devez entrer un nombre');
 	}
 	else {
-		if (age2 < 18) {
-			alert('Band train ou lé pas majeur ' + age2);
+		if (age < 18) {
+			alert('Hop ! Tu dois être majeur pour accéder à notre site');
 		}
 		else { 
-			window.location.href="lapage.html";
+			window.location.href="site.html";
 			// redirection();
-			alert('Lébon rent a ou ' + age2);
+			alert('Bravo ! Tu peux accéder à notre site de campagne !');
 			// alert('ok');
 			// redirection()
 			sessionStorage["session"] = "majeur";
 		}
 	}
 }
-
-
-
-
-// function redirection() {
-// 	// window.location.href="lapage.html";
-// 	// 1. Récupération du nombre entré
-// 	var age = document.forms.monFormulaire.nombre.value;
-// 	var age2 = document.getElementById("nombre").value;
-
-// 	// 2. Vérification des saisies de l'utilisateur
-// 	if (isNaN(age2)) {
-// 		alert('ce n\'est pas un chiffre');
-// 	}
-// 	else {
-// 		if (age2 < 18) {
-// 			alert('Band train ou lé pas majeur ' + age2);
-// 		}
-// 		else { 
-// 			window.location.href="lapage.html";
-// 			// redirection();
-// 			alert('Lébon rent a ou ' + age2);
-// 			// alert('ok');
-// 			// redirection()
-// 		}
-// 	}
-// }
-
-// window.onbeforeunload = function() {   
-// 	localStorage.removeItem(key);   
-// 	return ''; 
-// };
