4e357bd7abec8502e23e84a3195aa143cc69bd31
yannivador <yann901@hotmail.fr>
2022-01-14 11:01:06 +0400

    fin date

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
+function diffDate(date1, date2)
+{
+  return ((date1.getTime() - date2.getTime()) / 31536000000).toFixed(0);
+}
 
 
 function login() {
-	var age = document.getElementById("nombre").value;
+	var date_nais = document.getElementById("date_nais").value;
 
-	if (isNaN(age)) {
-		alert('Vous devez entrer un nombre');
+	var date_naiss = new Date(date_nais)
+	var today = new Date();
+
+	// age = ((today.getTime() - date_naiss.getTime()) / 31536000000).toFixed(0);
+	age = diffDate(today, date_naiss);
+
+	if (age < 18) {
+		alert('Hop la ! Tu dois être majeur pour accéder à notre site');
 	}
-	else {
-		if (age < 18) {
-			alert('Hop ! Tu dois être majeur pour accéder à notre site');
-		}
-		else { 
-			window.location.href="site.html";
-			// redirection();
-			alert('Bravo ! Tu peux accéder à notre site de campagne !');
-			// alert('ok');
-			// redirection()
-			sessionStorage["session"] = "majeur";
-		}
+	else { 
+		window.location.href="site.html";
+		// redirection();
+		alert('Bravo ! Tu peux accéder à notre site de campagne !');
+		// alert('ok');
+		// redirection()
+		sessionStorage["session"] = "majeur";
 	}
 }
